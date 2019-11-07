7024c7eecb6253887d0b839966f7054a81d34c90
= <samuel.sully@live.com>
2019-11-07 17:13:43 +0000

    Working version of ping RPC implemented.

--- Code/VoxelPopuli/rpc.py
+++ Code/VoxelPopuli/rpc.py
@@ -2,17 +2,19 @@ import asyncio
 import json
 import socket
 import random
+import sys
 from node import Node
 
 
+PYTHONASYNCIODEBUG = 1
 TIMEOUT = 10  # RPC timeout.
 
 
 def stub(func):
-    async def rpc_stub(self, *args):
+    async def rpc_stub(self, node, *args):
         loop = asyncio.get_event_loop()
-        msg = json.dumps({"id": random.getrandbits(32), "node": self.id, "call": True, "rpc": func.__name__, "arg": args})
-        self.socket.sendto(msg.encode("UTF-8"), self.addr)
+        msg = {"id": random.getrandbits(32), "node": self.id, "call": True, "rpc": func.__name__, "args": args}
+        self.transport.sendto(json.dumps(msg).encode("UTF-8"), node.addr)
         f = asyncio.Future()
         self.waiting[msg["id"]] = (f, loop.call_later(TIMEOUT, self._timeout, msg["id"]))
         await f
@@ -26,54 +28,55 @@ def rpc(func):
     return rpc_func
 
 
-class KademliaServer:
-    def __init__(self, node_id, addr):
+class KademliaServer(asyncio.DatagramProtocol):
+    def __init__(self, node_id):
         self.id = node_id
-        self.addr = addr
         self.waiting = {}
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.socket.bind(addr)
+        self.transport = None
 
-    async def run(self):
-        while True:
-            data,addr = self.socket.recvfrom(1024)
-            msg = json.loads(data.decode("UTF-8"))
-            node = Node(msg["node"], addr)
-            if msg["call"]:
-                func = getattr("_"+msg["rpc"], None)
-                if func is None or not callable(func) and func.__name__ == "rpc_func":
-                    continue
-                res = json.dumps({"id": msg["id"], "node": self.id, "call": False, "rpc": msg["rpc"], "ret": func(*msg["args"], node)})
-                self.socket.sendto(res.encode("UTF-8"), addr)
-            else:
-                if msg["id"] in self.waiting:
-                    self.waiting[msg["id"]][1].cancel()
-                    self.waiting[msg["id"]][0].set_result((True, msg["ret"]))
-                    del self.waiting[msg["id"]]
+    def connection_made(self, transport):
+        self.transport = transport
 
-    async def _timeout(self, msg_id):
+    def datagram_received(self, data, addr):
+        msg = json.loads(data.decode("UTF-8"))
+        print(msg)
+        node = Node(msg["node"], addr)
+        if msg["call"]:
+            func = getattr(self,"_"+msg["rpc"], None)
+            if func is None or not callable(func) or func.__name__ != "rpc_func":
+                return
+            res = json.dumps({"id": msg["id"], "node": self.id, "call": False, "rpc": msg["rpc"], "ret": func(*msg["args"], node)})
+            self.transport.sendto(res.encode("UTF-8"), addr)
+        else:
+            if msg["id"] in self.waiting:
+                self.waiting[msg["id"]][1].cancel()
+                self.waiting[msg["id"]][0].set_result((True, msg["ret"]))
+                del self.waiting[msg["id"]]
+
+    def _timeout(self, msg_id):
+        print("timed out")
         self.waiting[msg_id][0].set_result((False, None))
         del self.waiting[msg_id]
 
     @stub
-    async def ping(self):
+    async def ping(self, node):
         pass
 
     @stub
-    async def find_node(self, node_id):
+    async def find_node(self, node, node_id):
         pass
 
     @stub
-    async def find_value(self, key):
+    async def find_value(self, node, key):
         pass
 
     @stub
-    async def store(self, key, value):
+    async def store(self, node, key, value):
         pass
 
     @rpc
     def _ping(self, source):
-        pass
+        return self.id
 
     @rpc
     def _find_node(self, node_id, source):
@@ -86,3 +89,34 @@ class KademliaServer:
     @rpc
     def _store(self, key, value, source):
         pass
+
+
+async def main():
+    loop = asyncio.get_running_loop()
+
+    transport, protocol = await loop.create_datagram_endpoint(lambda: KademliaServer(0), local_addr=('127.0.0.1', 25565))
+
+    await asyncio.sleep(3600)
+
+
+async def test1(port1, port2):
+    loop = asyncio.get_running_loop()
+
+    transport, protocol = await loop.create_datagram_endpoint(lambda: KademliaServer(1), local_addr=('127.0.0.1', 25566))
+
+    print(await protocol.ping(Node(0,("localhost",25565))))
+
+
+async def test2(port1):
+    loop = asyncio.get_running_loop()
+
+    transport, protocol = await loop.create_datagram_endpoint(lambda: KademliaServer(2), local_addr=('127.0.0.1', 25567))
+
+
+if len(sys.argv) > 1:
+    if sys.argv[1] == "main":
+        asyncio.run(main())
+    elif sys.argv[1] == "test1":
+        asyncio.run(test1(random.randint(25500, 25600), 25568))
+    elif sys.argv[1] == "test2":
+        asyncio.run(test2(random.randint(25500, 25600)))
