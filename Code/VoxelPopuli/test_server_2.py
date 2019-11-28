c8703733d7a414f64254580d70b18908f652fe40
Sam Sully <samuel.sully@live.com>
2019-11-28 15:58:25 +0000

    Tweaked test

--- Code/VoxelPopuli/test_server_2.py
+++ Code/VoxelPopuli/test_server_2.py
@@ -8,11 +8,14 @@ logging.basicConfig(level=logging.DEBUG)
 
 async def test(port):
     with open("stats.dat","w") as f:
-        for i in range(0, 1):
+        x = [None]*100
+        for i in range(0, 100):
             k1 = Server(('127.0.0.1', port+i))
             await k1.run(Node(90000000000000000000000000000000000000000000000, ('127.0.0.1', 25499)))
+            x[i] = k1
+        for i in range(0, 100):
             t = time.time()
-            await k1.lookup()
+            await x[i].server.lookup(69)
             t = time.time() - t
             f.write(str(2+i) + ","+str(t)+"\n")
 
