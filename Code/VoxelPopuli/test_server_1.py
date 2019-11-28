50673eef768ad9f2ccdb3585580fcced2f918b10
Sam Sully <samuel.sully@live.com>
2019-11-28 10:55:40 +0000

    Push test to server

--- Code/VoxelPopuli/test_server_1.py
+++ Code/VoxelPopuli/test_server_1.py
@@ -1,7 +1,7 @@
 from server.server import Server
 from kademlia.routing import *
 from kademlia.node import Node
-import logging
+import logging, sys
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -10,6 +10,17 @@ async def test():
 
     await k1.run()
     print("yeet")
+
+    def got_input():
+        s = sys.stdin.readline()
+        if s.startswith("s"):
+            s=s[1:].split(",")
+            k1.server.set(s[0],s[1])
+        elif s.startswith("g"):
+            s=s[1:]
+            k1.serget.get(s)
+
+    asyncio.get_running_loop().add_reader(sys.stdin, got_input)
 
     while True:
         await asyncio.sleep(5)
