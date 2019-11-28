a56de1fdd3e2ace44335414b8c0d864073ba9c1a
Sam Sully <samuel.sully@live.com>
2019-11-28 14:50:58 +0000

    Push some tests

--- Code/VoxelPopuli/test_server_2.py
+++ Code/VoxelPopuli/test_server_2.py
@@ -1,14 +1,20 @@
 from server.server import Server
 from kademlia.node import Node
 from kademlia.routing import *
-import logging
+import logging, time
 
 logging.basicConfig(level=logging.DEBUG)
 
+
 async def test(port):
-    for i in range(0,50):
-        k1 = Server(('127.0.0.1', port+i))
-        await k1.run(Node(90000000000000000000000000000000000000000000000, ('127.0.0.1', 25499)))
+    with open("stats.dat","w") as f:
+        for i in range(0, 1):
+            k1 = Server(('127.0.0.1', port+i))
+            await k1.run(Node(90000000000000000000000000000000000000000000000, ('127.0.0.1', 25499)))
+            t = time.time()
+            await k1.lookup()
+            t = time.time() - t
+            f.write(str(2+i) + ","+str(t)+"\n")
 
     while True:
         await asyncio.sleep(3600)
