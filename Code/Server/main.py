15766d91de62a29318958796e754ca50daa065d0
Sam Sully <samuel.sully@live.com>
2020-01-25 17:21:21 +0000

    tweak

--- Code/Server/main.py
+++ Code/Server/main.py
@@ -80,8 +80,6 @@ def ctrl_loop():
     t = time.monotonic()
 
     while True:
-        if time.monotonic() - t > 10:
-            print(dht.id)
         if time.monotonic() - t > 3600:
             for coord in chunks:
                 asyncio.run_coroutine_threadsafe(dht.republish_chunk(coord, (bind_ip, base_port + 1)), dht.loop)
