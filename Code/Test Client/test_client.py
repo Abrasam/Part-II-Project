5efc8e87344659713d25914d877d4efbbbac539f
Sam Sully <samuel.sully@live.com>
2020-04-16 20:11:59 +0100

    tweak client

--- Code/Test Client/test_client.py
+++ Code/Test Client/test_client.py
@@ -63,7 +63,7 @@ clients = []
 
 for i in range(num_clients):
     print(f"[{i}/{num_clients}] ",end="")
-    c = Client(0,0,random.randint(0,500)*i)
+    c = Client(0,0,random.randint(1,5000)*i)
     c.move()
     clients.append(c)
 
