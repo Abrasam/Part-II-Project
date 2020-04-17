44599d36a40f4fa68240cc000838e552ead0d66a
Sam Sully <samuel.sully@live.com>
2020-04-17 10:59:51 +0100

    Create test_client_auxillary.py

---
+++ Code/Test Client/test_client_auxillary.py
@@ -0,0 +1,47 @@
+import sys, socket, json, random, time
+
+if len(sys.argv) < 2:
+    print("Required arguments: <number of clients> <node ip> <node port> <chunkx> <chunky>")
+    sys.exit(-1)
+
+CHUNK_SIZE = 32
+
+num_clients = int(sys.argv[1])
+addr = (sys.argv[2], int(sys.argv[3]))
+clients = []
+
+chunkx,chunky=int(sys.argv[4]),int(sys.argv[5])
+
+class Client:
+    def __init__(self,x,y,i,s):
+        self.server = None
+        self.x,self.y = x,y
+        self.server = s
+        self.id = i
+        self.init = False
+
+    def update(self):
+        if not self.init:
+            self.server.send(json.dumps({"type":1,"args":[CHUNK_SIZE*self.x,CHUNK_SIZE*self.y],"player":f"testplayer{self.id}"}).encode() + b'\n')
+            self.init = True
+        try:
+            self.server.recv(102400)
+        except:
+            pass
+        self.server.send(json.dumps({"type":3,"args":[CHUNK_SIZE*self.x+CHUNK_SIZE/2+random.randint(-CHUNK_SIZE/4,CHUNK_SIZE/4),CHUNK_SIZE+1,CHUNK_SIZE*self.y+CHUNK_SIZE/2+random.randint(-CHUNK_SIZE/4,CHUNK_SIZE/4),random.random()*3.14],"player":f"testplayer{self.id}"}).encode() + b'\n')
+
+for i in range(num_clients):
+    print(f"{i+1}/{num_clients}")
+    ii = random.getrandbits(16)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(addr)
+    s.send(json.dumps({"type":"connect","player":f"{ii}","chunk":(chunkx,chunky)}).encode())
+    ok = s.recv(2)
+    if ok != b'ok':
+        print("Error",file=sys.stderr)
+    else:
+        clients.append(Client(chunkx,chunky,ii,s))
+while True:
+    for c in clients:
+        c.update()
+    time.sleep(1/20.)
